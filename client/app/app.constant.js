550ace8910c2a1751928177f75d64d0abc30a84f
leekangtaqi <leekangtaqi@sina.com>
2016-12-20 20:17:22 +0800

    v5.1.1 build

--- client/app/app.constant.js
+++ client/app/app.constant.js
@@ -2,11 +2,11 @@
   angular.module("clientApp.constants", [])
 
 .constant("appConfig", {
-	"uri": "http://apidev.91songli.cc",
+	"uri": "http://api.91songli.cc",
 	"imgUri": "img.91pintuan.com",
 	"phtUri": "photo.91pintuan.com",
 	"phtUriExotic": "http://photo.91pintuan.com",
-	"component": "5726bf8700bbe21526c4ccbe",
+	"component": "5581117b5f225e4c401c9259",
 	"apiUri": {
 		"acd5e6e0ab2c366ea83d21c106c07912": "http://api.91songli.cc",
 		"4fbe2e7d1ce5245067a9a82ea8f04bb5": "http://api.91songli.com",
@@ -15,7 +15,7 @@
 		"4f3aee5014f4f60f0eb7bbf10e621646": "http://apidev.91songli.cc",
 		"887e3c18637d78c3a747e37f77f7c8a9": "http://apidev.91songli.cc"
 	},
-	"debug": true,
+	"debug": false,
 	"from": "client"
 })
 
